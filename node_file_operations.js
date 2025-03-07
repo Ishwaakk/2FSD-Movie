5bc0e86f6246ca42af9d9e0594dd57d6f82ee68c
Ishwaakk <ishwaak.saraogi@bcah.christuniversity.in>
2025-03-07 14:47:52 +0530

    lab-9 consolidated

---
+++ node_file_operations.js
@@ -0,0 +1,72 @@
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname, 'moviesData.txt');
+
+// Create a file
+function createFile() {
+    fs.writeFile(filePath, 'Movie List:\n', (err) => {
+        if (err) throw err;
+        console.log('File created successfully!');
+    });
+}
+
+// Open (Conceptual - just checks if the file exists)
+function openFile() {
+    fs.open(filePath, 'r', (err, fd) => {
+        if (err) {
+            console.error('Error opening file:', err.message);
+            return;
+        }
+        console.log('File opened successfully!');
+        fs.close(fd, () => {});
+    });
+}
+
+// Read from the file
+function readFile() {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error reading file:', err.message);
+            return;
+        }
+        console.log('File contents:\n', data);
+    });
+}
+
+// Write to the file
+function writeFile() {
+    const movie = 'Ishwaak s FSD Works!! - Sci-Fi - Rating: 8.8\n';
+    fs.appendFile(filePath, movie, (err) => {
+        if (err) throw err;
+        console.log('Movie data written to file!');
+    });
+}
+
+// Close file (Conceptual - files auto-close in Node.js)
+function closeFile() {
+    console.log('Files are automatically closed after operations in Node.js');
+}
+
+// Command line interface
+const command = process.argv[2];
+
+switch (command) {
+    case 'create':
+        createFile();
+        break;
+    case 'open':
+        openFile();
+        break;
+    case 'read':
+        readFile();
+        break;
+    case 'write':
+        writeFile();
+        break;
+    case 'close':
+        closeFile();
+        break;
+    default:
+        console.log('Usage: node node_file_operations.js [create|open|read|write|close]');
+}
